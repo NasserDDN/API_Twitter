d1bac44ed1af8faf9e5c4662037db4ab499671da
Nasser DANDANA <95056337+HakimCodage@users.noreply.github.com>
2023-03-10 08:50:50 +0000

    Creation du frontend en vue et de ses dépendances

---
+++ frontend/src/Twitter.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <div id="twitter">
+    {{ tweets }}
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            tweets: 'No Tweets',
+        };
+    },
+};
+</script>
---
+++ frontend/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './Twitter.vue';
+
+new Vue({
+    el: '#twitter',
+    render: h => h(App),
+});
